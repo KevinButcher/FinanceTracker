abf90f19312d0a22390bb4f78aefa9a6b8cd850c
KevinButcher <kevinbutcher@mail.weber.edu>
2024-11-14 13:21:33 -0700

    modified user email requirement and auto-create profile on user creation

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver # simplify connecting functions to signals
+from django.db.models.signals import post_save # execute a function after something is executed
+from django.conf import settings
 
 # Create your models here.
+
+# make it so the email fields can't be blank and that the email is unique
+User._meta.get_field('email').blank = False
+User._meta.get_field('email').null = False
+User._meta.get_field('email')._unique = True
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,3 +18,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+# signal to automatically create a profile instance after a user is created, receiver specifies when to ran after receiving signal
+@receiver(post_save, sender=settings.AUTH_USER_MODEL) # settings.AUTH_USER_MODEL allows for referencing the user model configured in settings.py
+def auto_create_profile(sender, instance, created, **kwargs): # kwargs stands for keyword arguments (instance, created, etc.)
+    if created:
+        Profile.objects.get_or_create(user=instance)
